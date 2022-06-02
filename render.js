0f734f8080eb1e9b2c8145c05db041452a335977
Ksenia PI <101118611+ksalpern@users.noreply.github.com>
2022-06-02 19:00:40 +0300

    feat: fixed dialogsPage, 42 lessons done

---
+++ render.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import App from './App';
+//import { addPost, updateNewPostText } from './redux/state';
+//import { BrowserRouter} from 'react-router-dom';
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//export let rerenderEntireTree = (state) => {
+  
+  //root.render(
+    //<BrowserRouter>
+     // <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+   // </BrowserRouter>
+  //);
+//}
+
